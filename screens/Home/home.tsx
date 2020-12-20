7c1fbb198305f8941b9986270f5845b8b5edfa12
Kanishka Narjary <kanishkan14@gmail.com>
2020-12-20 23:31:09 +0530

    calendar setup

--- screens/Home/home.tsx
+++ screens/Home/home.tsx
@@ -1,10 +1,40 @@
 import * as React from 'react';
+import {useState} from 'react';
 import {Text, View} from 'react-native';
+import {Calendar} from 'react-native-calendars';
 
 const Home: React.FC = () => {
+  const [currDate, setCurrDate] = useState(
+    new Date().getFullYear() +
+      '-' +
+      (new Date().getMonth() + 1) +
+      '-' +
+      new Date().getDate(),
+  );
   return (
     <View>
       <Text>Home</Text>
+      <Calendar
+        onDayPress={(day) => {
+          setCurrDate(day.dateString);
+          console.log('selected date', day.dateString);
+        }}
+        onDayLongPress={(day) => {
+          setCurrDate(day.dateString);
+          console.log('selected day', day);
+        }}
+        monthFormat={'MMM yyyy'}
+        onMonthChange={(month) => {
+          console.log('month changed', month);
+        }}
+        disableMonthChange={true}
+        firstDay={1}
+        onPressArrowLeft={(subtractMonth) => subtractMonth()}
+        onPressArrowRight={(addMonth) => addMonth()}
+        enableSwipeMonths={true}
+      />
+      <Text>Today's Date:</Text>
+      <Text>{currDate}</Text>
     </View>
   );
 };
